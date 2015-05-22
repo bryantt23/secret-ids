2770d8ed85ac508168859a3bbe5fbc02f156b291
bryantt23 <bryantt23@gmail.com>
2015-05-22 04:47:14 -0700

    basic crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,51 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all.order('created_at ASC')
-	end
 
-	def new
-	end
+  def index
+    @posts = Post.all.order('created_at ASC')
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = Post.new(post_params)
+  def show
+    @post = Post.find(params[:id])
+  end
 
-		@post.save
-		redirect_to @post
-	end
+  def create
+    @post = Post.new(post_params)
 
-	private
+    		if @post.save
+    			redirect_to @post
+    		else
+    			render 'new'
+    		end
+  end
 
-	def post_params
-		params.require(:post).permit(:name, :secret)
-	end
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:name, :secret))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :secret)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+	validates :name, presence: true, length: {minimum: 3}
+	validates :secret,  presence: true
 end
