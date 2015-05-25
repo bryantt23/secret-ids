0e9749e3740efafce8539986a7494ba8181e44dc
bryantt23 <bryantt23@gmail.com>
2015-05-25 04:47:49 -0700

    mini icons working

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,8 @@ class Post < ActiveRecord::Base
 
 
 	validates :avatar, presence: true
-	has_attached_file :avatar, styles: { :medium => "400x400#" }
+	has_attached_file :avatar, styles: { :medium => "400x400#" , :thumb => "100x100#" }
+	# has_attached_file :avatar, styles: { :medium => "400x400#" }
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 
